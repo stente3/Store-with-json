8e115762107305600f9d3ecfdee90c59289ca178
stente3 <stentem0103@gmail.com>
2021-12-13 10:05:14 -0500

    Fetch API was added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,17 @@
 /* Variables */
 let containerItems = document.querySelector(".container__items")
-const requestURL = "https://raw.githubusercontent.com/stente3/Store-with-json/main/tshirts.json";
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
+const data = "https://raw.githubusercontent.com/stente3/Store-with-json/main/tshirts.json";
+fetch(data)
+    .then(data => data.json())
+    .then(data => showProducts(data));
+
 
 /* Functions */
-request.onload = () => {
-    const products = request.response.items;
-    products.forEach(element => {
+function showProducts(products) {
+    products.items.forEach(element => {
         let item = document.createElement("div");
         item.classList.add("col-sm-6", "col-xl-4", "mb-3");
-        /* Create a card with all the information */
+        /* Create a card with all the information*/
         item.innerHTML = `<div class="card">
                             <img src="${element.location}" class="card-img-top" alt="T-shirts">
                             <div class="card-body d-flex flex-column align-items-center">
@@ -24,5 +23,3 @@ request.onload = () => {
         containerItems.appendChild(item);
     });
 }
-
-
